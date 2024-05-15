32b2dfab1880fcab6dcee0a2068c08130fb6435d
Rohan Tangri <rohan.tangri@trili.tech>
2024-05-15 15:26:09 +0100

    add config files

---
+++ layerzero.config.ts
@@ -0,0 +1,36 @@
+import { EndpointId } from '@layerzerolabs/lz-definitions'
+
+import type { OAppOmniGraphHardhat, OmniPointHardhat } from '@layerzerolabs/toolbox-hardhat'
+
+const etherlinkTestnetContract: OmniPointHardhat = {
+    eid: EndpointId.ETHERLINK_V2_TESTNET,
+    contractName: "WXTZToken"
+}
+
+const sepoliaContract: OmniPointHardhat = {
+    eid: EndpointId.SEPOLIA_V2_TESTNET,
+    contractName: 'WXTZToken',
+}
+
+const config: OAppOmniGraphHardhat = {
+    contracts: [
+        {
+            contract: etherlinkTestnetContract,
+        },
+        {
+            contract: sepoliaContract,
+        },
+    ],
+    connections: [
+        {
+            from: etherlinkTestnetContract,
+            to: sepoliaContract,
+        },
+        {
+            from: sepoliaContract,
+            to: etherlinkTestnetContract,
+        },
+    ],
+}
+
+export default config
