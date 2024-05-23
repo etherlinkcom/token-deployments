792dc66530a7a8b87b64fc0d630e0dbabd26104d
Camille Bouzerand <bcamille99@gmail.com>
2024-05-23 12:26:02 +0100

    [FIX] clean tzBTC to just have an ERC20 upgradable contract

--- contracts/tzBTCToken.sol
+++ contracts/tzBTCToken.sol
@@ -1,28 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.22;
 
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import { OFT } from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/OFT.sol";
-import { ERC20PermitUpgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
-import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-
-contract tzBTCToken is OwnableUpgradeable, ERC20PermitUpgradeable, UUPSUpgradeable, OFT {
+contract tzBTCToken is Initializable, ERC20Upgradeable, ERC20PermitUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
     string private constant _name = "Tezos Bitcoin";
     string private constant _symbol = "tzBTC";
 
     event Mint(address indexed to, uint amount);
     event Burn(address indexed to, uint amount);
 
-    constructor(address _lzEndpoint, address _delegate) OFT(_name, _symbol, _lzEndpoint, _delegate) {
+    constructor() {
         _disableInitializers();
     }
 
     function initialize(address _owner) initializer public {
         __ERC20_init(_name, _symbol);
-        __Ownable_init(_owner);
         __ERC20Permit_init(_name);
+        __Ownable_init(_owner);
         __UUPSUpgradeable_init();
     }
 
