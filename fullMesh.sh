92f586e99d23184e7a5ff2b32e26c40ec5cbf38b
Camillebzd <bcamille99@gmail.com>
2024-10-03 11:32:55 +0100

    [ADD] bash script to setup the full mesh on WXTZ

---
+++ fullMesh.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# List of networks, no Etherlink because already setup
+networks=("mainnet" "arbitrumOne" "base" "bsc")
+
+# Loop through each combination of networks
+for (( i=0; i<${#networks[@]}; i++ )); do
+  for (( j=i+1; j<${#networks[@]}; j++ )); do
+    # Assign target and source networks
+    network_A=${networks[$i]}
+    network_B=${networks[$j]}
+
+    # Run the command with A as target and B as source
+    echo "Running: targetNetworkName=$network_A npx hardhat run --network $network_B scripts/setPeer.ts"
+    targetNetworkName=$network_A npx hardhat run --network $network_B scripts/setPeer.ts
+
+    # Run the command with B as target and A as source
+    echo "Running: targetNetworkName=$network_B npx hardhat run --network $network_A scripts/setPeer.ts"
+    targetNetworkName=$network_B npx hardhat run --network $network_A scripts/setPeer.ts
+  done
+done
